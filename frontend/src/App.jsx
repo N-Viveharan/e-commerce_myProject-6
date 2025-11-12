0de853cc2e0cb12cbd5637b68de1c53233da9bab
N-Viveharan <nagalingamviveharan2001@gmail.com>
2025-11-12 20:50:27 +0530

    create the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Project from './Pages/Project';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/product' element={<Project/>}>
+          <Route path=':product' element={<Project/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   )
 }
