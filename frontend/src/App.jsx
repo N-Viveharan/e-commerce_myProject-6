f06a696fefae0a87a00803b5c2f7c11fd4e357c3
N-Viveharan <nagalingamviveharan2001@gmail.com>
2025-11-14 20:52:59 +0530

    modify the change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,10 @@ import ShopCategory from './Pages/ShopCategory';
 import Project from './Pages/Project';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
+import Fooder from './Components/Fooder/Fooder';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
 function App() {
   return (
     <div>
@@ -13,9 +17,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
         <Route path='/product' element={<Project/>}>
           <Route path=':product' element={<Project/>}/>
         </Route>
@@ -23,6 +27,7 @@ function App() {
         <Route path='/login' element={<LoginSignup/>}/>
 
       </Routes>
+      <Fooder/>
       </BrowserRouter>
       
     </div>
