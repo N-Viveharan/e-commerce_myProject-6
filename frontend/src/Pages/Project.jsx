432dfeebf83802ca4734242e8239af05ba4e9b29
N-Viveharan <nagalingamviveharan2001@gmail.com>
2025-11-17 21:52:29 +0530

    create the cart detail

--- frontend/src/Pages/Project.jsx
+++ frontend/src/Pages/Project.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useLocation } from 'react-router-dom'
+import Breadcrums from '../Components/Breadcrums/Breadcrums'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 function Project() {
-  
+  const { all_product } = useContext(ShopContext);
+  const location = useLocation();
+
+
+  const urlParts = location.pathname.split('/');
+  const productId = Number(urlParts[urlParts.length - 1]);
+
+  const product = all_product.find((e) => e.id === productId);
+
   return (
-    <div>Project</div>
-  )
+    <div>
+      <Breadcrums product={product} />
+      <ProductDisplay product={product}/>
+    </div>
+  );
 }
 
-export default Project
+export default Project;
