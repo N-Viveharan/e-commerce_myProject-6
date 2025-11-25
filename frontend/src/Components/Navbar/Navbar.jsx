aa5af3a079b2a909057ad96606d241ff11069c57
N-Viveharan <nagalingamviveharan2001@gmail.com>
2025-11-25 21:27:36 +0530

    create the portable

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,20 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/nav_dropdown.png'
 
 function Navbar() {
   
   const [menu,setMenu]=useState("shop")
 const {getTotalCartItems}=useContext(ShopContext)
+const menuRef =useRef()
+const dropdown_toggle=(e)=>{
+menuRef.current.classList.toggle('nav-menu-visible')
+e.target.classList.toggle('open')
+}
 
   return (
     <div className="navbar">
@@ -16,7 +22,8 @@ const {getTotalCartItems}=useContext(ShopContext)
             <img src={logo} alt="" />
             <p>SHOPPER</p>
         </div>
-        <ul className="nav-menu">
+        <img src={nav_dropdown} onClick={dropdown_toggle} className='nav-dropdown' alt="" />
+        <ul  ref={menuRef}  className="nav-menu">
             <li onClick={()=>setMenu("shop")}><Link to='/' style={{textDecoration:'none'}}>Shop</Link> {menu==="shop"?<hr/>:<></>}</li>
             <li onClick={()=>setMenu("men")}><Link to='/mens' style={{textDecoration:'none'}}>Men</Link> {menu==="men"?<hr/>:<></>}</li>
             <li onClick={()=>setMenu("women")}><Link to='/womens' style={{textDecoration:'none'}}>Women</Link> {menu==="women"?<hr/>:<></>}</li>
