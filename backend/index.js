1effb36eb3c0128b1b2bfb31697e5a2ae24bb341
N-Viveharan <nagalingamviveharan2001@gmail.com>
2025-12-11 22:25:31 +0530

    connect mongodb

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,39 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+
+mongoose.connect("mongodb+srv://nagalingamviveharan2001_db_user:project123@cluster0.alygd35.mongodb.net/?appName=Cluster0")
+
+//image store
+const storage=multer.diskStorage({
+    destination:'./upload/images',
+    filename:(req,file,cd)=>{
+        return cd(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload=multer({storage:storage})
+
+
+
+app.use('/images', express.static('upload/images'))
+
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server Running on Port " + port)
+    } else {
+        console.log("Error : " + error)
+    }
+})
