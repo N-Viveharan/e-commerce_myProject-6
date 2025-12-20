1810c66e8b2eea86efb230d504bfcc4bf7d2c387
N-Viveharan <nagalingamviveharan2001@gmail.com>
2025-12-20 23:10:32 +0530

    create the  list product

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -3,8 +3,8 @@ import './ListProduct.css'
 function ListProduct() {
   return (
     <div>
-      <div>
-        
+      <div className='list-product'>
+        <h1>All products list</h1>
       </div>
     </div>
   )
