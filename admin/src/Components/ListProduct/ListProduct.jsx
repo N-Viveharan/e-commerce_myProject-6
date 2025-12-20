88476020ed5e82eaea27e25ccf460f121ac21100
N-Viveharan <nagalingamviveharan2001@gmail.com>
2025-12-20 23:12:49 +0530

    edit  the  list product

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -5,6 +5,14 @@ function ListProduct() {
     <div>
       <div className='list-product'>
         <h1>All products list</h1>
+        <div className="listproduct-format-main">
+           <p>Products</p>
+<p>Title</p>
+<p>Old Price</p>
+<p>New Price</p>
+<p>Category</p>
+<p>Remove</p>
+        </div>
       </div>
     </div>
   )
