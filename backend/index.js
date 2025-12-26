98e3a1390bc9b3dd808dc5a57a17cf00accbab32
N-Viveharan <nagalingamviveharan2001@gmail.com>
2025-12-26 22:22:25 +0530

    create the signup for the admin

--- backend/index.js
+++ backend/index.js
@@ -154,6 +154,39 @@ const Users = mongoose.model('Users',{
     }
 })
 
+// Creating Endpoint for registering the user
+app.post('/signup', async (req, res) => {
+
+    let check = await Users.findOne({ email: req.body.email });
+    if (check) {
+        return res.status(400).json({ success: false, errors: "existing user found with same email" })
+    }
+
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0;
+    }
+
+    const user = new Users({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart,
+    })
+
+    await user.save();
+
+    const data = {
+        user: {
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_ecom');
+    res.json({ success: true, token })
+
+})
+
 // Start server
 app.listen(port, (error) => {
     if (!error) {
